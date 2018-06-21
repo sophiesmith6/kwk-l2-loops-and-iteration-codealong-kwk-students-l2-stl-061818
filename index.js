b768146b248e4b53d8091d0941d8442fd600150d
sophiesmith6 <18smiths@corjesu.org>
2018-06-21 16:33:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solutions in this file
+function tailsNeverFail() {
+  let tails = 0;
+  
+  while (Math.random() >= 0.5)
+    tails++
+  }
+  
+  return  `You got ${tails} tails in a row!`;
+}
