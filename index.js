11b1bd61531725a5011eee0c48ec1aaa7902b683
sophiesmith6 <18smiths@corjesu.org>
2018-06-21 16:36:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 function tailsNeverFails() {
   let tails = 0;
   
-  while (Math.random() >= 0.5)
-    tails++;
+  while (Math.random() >= 0.5) {
+    tails++
   }
   
   console.log(`You got ${tails} tails in a row!`);
